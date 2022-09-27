80c7bf05f1804d51e34ccdee9eb96e915a21ae56
Entwicklung <entwicklung@h2-invent.com>
2022-09-27 08:43:45 +0200

    fix public sheduling not sending results to server

--- assets/js/fullcalendar.js
+++ assets/js/fullcalendar.js
@@ -1,26 +1,31 @@
-import { Calendar } from '@fullcalendar/core';
+import {Calendar} from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import deLocale from '@fullcalendar/core/locales/de';
 import bootstrapPlugin from '@fullcalendar/bootstrap';
+
 function initCalendar() {
     var calendarEl = document.getElementById('calendar');
-    var calendar = new Calendar(calendarEl, {
-        plugins: [ bootstrapPlugin , dayGridPlugin, timeGridPlugin, listPlugin ],
-        locale: deLocale,
-        themeSystem: 'bootstrap',
-        headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,listMonth'
-        },
-        navLinks: true, // can click day/week names to navigate views
-        editable: false,
-        dayMaxEvents: true, // allow "more" link when too many events
-        events: events
-    });
+    if (calendarEl && typeof events !== 'undefined') {
+        var calendar = new Calendar(calendarEl, {
+            plugins: [bootstrapPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
+            locale: deLocale,
+            themeSystem: 'bootstrap',
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,listMonth'
+            },
+            navLinks: true, // can click day/week names to navigate views
+            editable: false,
+            dayMaxEvents: true, // allow "more" link when too many events
+            events: events
+        });
 
-    calendar.render();
+        calendar.render();
+
+    }
 }
+
 export {initCalendar}
