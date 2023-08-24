cd0f9022a60bdd80f826bdb5cf4316c346d9d484
Entwicklung <entwicklung@h2-invent.com>
2023-08-24 08:36:29 +0200

    save name when enter into ShareLinkController.php

---
+++ src/Migrations/Version20230725204123.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230725204123 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE rooms ADD send_email_on_register TINYINT(1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE rooms DROP send_email_on_register');
+    }
+}
